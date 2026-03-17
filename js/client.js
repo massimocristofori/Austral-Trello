e11a76f7841e4580a9b92a0777eed37788a0b273
massimocristofori <massimo.cristofori@gmail.com>
2026-03-17 13:54:41 +0100

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,33 +1,40 @@
 window.TrelloPowerUp.initialize({
-  'list-actions': function(t, options) {
+  'list-actions': function(t) {
     return [{
-      text: 'Generate TA',
+      text: 'Export Tr. Agenda',
       callback: async function(t) {
-        const list = await t.list('id', 'name');
-        const cards = await t.cards('name', 'idList', 'idBoard');
+        try {
+          const list = await t.list('id', 'name');
+          const cards = await t.cards('name', 'idList', 'badges');
 
-        // Filter only cards in this list that are from a different board
-        const linkedCards = cards.filter(c => c.idList === list.id && c.idBoard !== list.idBoard);
+          const filtered = cards.filter(c => c.idList === list.id);
 
-        if (linkedCards.length === 0) {
-          alert('No linked cards in this list');
+          if (!filtered.length) {
+            alert('No cards in this list');
+            return t.closePopup();
+          }
+
+          function getCardName(c) {
+            if (c.badges && c.badges.attachments) {
+              const linked = c.badges.attachments.find(a => a.type === 'card');
+              if (linked) return linked.name;
+            }
+            return c.name;
+          }
+
+          const csv = ['Card Name', ...filtered.map(c => `"${getCardName(c).replace(/"/g,'""')}"`)].join('\n');
+
+          const blob = new Blob([csv], { type: 'text/csv' });
+          const url = URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = `${list.name}-linked-cards.csv`;
+          a.click();
+
           return t.closePopup();
+        } catch (err) {
+          alert('Error exporting linked cards: ' + err.message);
         }
-
-        // CSV
-        const header = 'Card Name';
-        const rows = linkedCards.map(c => `"${c.name.replace(/"/g, '""')}"`);
-        const csv = [header, ...rows].join('\n');
-
-        // Download CSV
-        const blob = new Blob([csv], { type: 'text/csv' });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = `${list.name}-linked-cards.csv`;
-        a.click();
-
-        return t.closePopup();
       }
     }];
   }
