39f91c871e08a5a4d6fde4da6d31aad52e184595
massimocristofori <massimo.cristofori@gmail.com>
2026-03-17 11:03:32 +0100

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,10 +1,15 @@
+// js/client.js
+
+console.log('Power-Up loaded'); // <-- This confirms the Power-Up is being loaded by Trello
+
 window.TrelloPowerUp.initialize({
   'board-buttons': function(t, options) {
     return [{
-      text: 'Export Transportation Agenda',
+      text: 'Export List CSV',
       callback: function(t) {
+        console.log('Board button clicked'); // <-- Logs when the button is pressed
         return t.popup({
-          title: 'Export TA',
+          title: 'Export List',
           url: 'export.html',
           height: 200
         });
